e993caa25028a49757322e2588a51edfada31508
Robert Nairn <robert.nairn.2022@uni.strath.ac.uk>
2024-02-21 10:14:13 +0000

    updating unit + integration tests

--- src/test/java/uk/co/asepstrath/bank/IntegrationTest.java
+++ src/test/java/uk/co/asepstrath/bank/IntegrationTest.java
@@ -1,13 +1,30 @@
 package uk.co.asepstrath.bank;
 
 import io.jooby.test.JoobyTest;
+import uk.co.asepstrath.bank.App;
+import io.jooby.StatusCode;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @JoobyTest(App.class)
 public class IntegrationTest {
-    /*
-    Integration tests should be here
-    Example can be found in example/IntegrationTest.java
-     */
+    static OkHttpClient client = new OkHttpClient();
+
+    @Test
+    public void shouldSayHi(int serverPort) throws IOException {
+        Request req = new Request.Builder()
+                .url("http://localhost:" + serverPort+"/example")
+                .build();
+
+        try (Response rsp = client.newCall(req).execute()) {
+            assertEquals("Welcome to Jooby!", rsp.body().string());
+            assertEquals(StatusCode.OK.value(), rsp.code());
+        }
+    }
 }
--- src/test/java/uk/co/asepstrath/bank/UnitTest.java
+++ src/test/java/uk/co/asepstrath/bank/UnitTest.java
@@ -1,10 +1,19 @@
 package uk.co.asepstrath.bank;
 
+import io.jooby.test.MockRouter;
+import uk.co.asepstrath.bank.App;
+import io.jooby.StatusCode;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UnitTest {
-    /*
-    Unit tests should be here
-    Example can be found in example/UnitTest.java
-     */
+    @Test
+    public void welcome() {
+        MockRouter router = new MockRouter(new App());
+        router.get("/example", rsp -> {
+            assertEquals("Welcome to Jooby!", rsp.value());
+            assertEquals(StatusCode.OK, rsp.getStatusCode());
+        });
+    }
 }
