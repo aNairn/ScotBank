c686bc91efca42d9b90324b607f791ae1b1b16e3
Sean Green <seangreen@Seans-MacBook-Pro-2.local>
2024-02-21 15:23:31 +0000

    Update Mockito Dependency and add JSON.get

--- src/main/java/uk/co/asepstrath/bank/App.java
+++ src/main/java/uk/co/asepstrath/bank/App.java
@@ -56,7 +56,7 @@ public class App extends Jooby {
             try {
                 //Create HttpClient
                 HttpClient client = HttpClient.newHttpClient();
-                //Creare GET request
+                //Create GET request
                 HttpRequest request = HttpRequest.newBuilder().uri(URI.create(apiUrl)).header("Accept", "application/json").build();
                 //Send/Receive
                 HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
@@ -74,12 +74,14 @@ public class App extends Jooby {
                         boolean roundUpEnabled = accountJson.getBoolean("roundUpEnabled");
                         // Create an Account object using the constructor for API data
                         Account account = new Account(UUID.fromString(id), name, startingBalance, roundUpEnabled);
-                        System.out.println("Parsed JSON Array:\n" + jsonArray);
+                        System.out.println("Parsed JSON Array:\n" + jsonArray.get(i));
 
                         // Add the account to your accounts list
                         accounts.add(account);
                         System.out.println("Adding account: " + accounts.get(i));
                     }
+
+                    System.out.println("The Selected Account: " + accounts.get(2));
 
 
                 } else {
